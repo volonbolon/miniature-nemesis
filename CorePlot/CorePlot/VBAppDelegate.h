474076a45a60074d7f02ca2452455d6afb2add5e
Ariel Rodriguez <ariel@volonbolon.net>
2014-02-04 12:12:57 -0300

    Add CorePlot project

---
+++ CorePlot/CorePlot/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  CorePlot
+//
+//  Created by Ariel Rodriguez on 2/4/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CorePlot/CorePlot/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  CorePlot
+//
+//  Created by Ariel Rodriguez on 2/4/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
